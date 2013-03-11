ea7e070148ce5ebae4cdd0c0fa70862e08733fba
Spencer Alger <spencer@spenceralger.com>
2013-03-11 00:18:10 -0700

    expanded the readme a bit, and added some tests

--- README.md
+++ README.md
@@ -1,4 +1,33 @@
-ZeptoDOM
-========
+# ZeptoDOM
 
-NPM Module for easily accessing the contents of a page with Zepto.
+ZeptoDOM or zdom is a simple Node.js module for accessing the contents of pages using
+[Zepto 1.0](https://github.com/madrobby/zepto/) or
+[jQuery 1.9.1](https://github.com/jquery/jquery/).
+
+Currently zdom simply wraps jsdom. It was developed to as a piece of a crawler
+I'm working on and is designed to have a small features set.
+
+## usage:
+
+```JavaScript
+var zdom = require('zdom');
+
+var tab = new zdom.tab();
+
+tab
+  .set('library', 'jquery') // the default is 'zepto', anything should be a [jsdom scripts arg](https://github.com/tmpvar/jsdom#how-it-works)
+  // TODO .set('follow redirects', true)
+  // TODO .set('user agent', 'chrome')
+  .navigate('http://google.com') // returns a q promise
+  .then(function ($, window) {
+    tab
+      .navigate($('.result').get(0).href)
+      .then(function(){
+        //down the rabbit hole you go
+      })
+  })
+  .fail(function (error) {
+    console.log('fall back, fall back!');
+  });
+
+```
--- tab.js
+++ tab.js
@@ -1,23 +1,59 @@
+"use strict";
 
 var Q         = require('q');
 var jsdom     = require("jsdom");
 var fs        = require('fs');
 var path      = require('path');
-var zeptoSrc  = fs.readFileSync(path.resolve(__dirname, "./zepto.js")).toString();
+var srcCache  = {};
 
 function Tab () {
 
+  this.set('library', 'zepto');
+
 }
+
+Tab.prototype = {
+  libSrc: null
+};
+
+Tab.prototype.set = function (option, value) {
+  var action = 'setOpt '+option;
+  this[action] && this[action].call(this, value);
+  return this;
+}
+
+Tab.prototype['setOpt library'] = function (lib) {
+  switch(lib) {
+    case 'jquery':
+    case 'zepto':
+      this.libSrc = srcCache[lib] || (srcCache[lib] = fs.readFileSync(path.resolve(__dirname, './libs/'+lib+'.js')).toString());
+      break;
+    default:
+      this.libSrc = null;
+      this.lib = lib;
+      break;
+  }
+};
+
+Tab.prototype['setOpt follow redirects'] = function () {
+  throw new Error('not implemented');
+};
+
+Tab.prototype['setOpt user agent'] = function () {
+  throw new Error('not implemented');
+};
+
 
 Tab.prototype.navigate = function(url) {
   var defr = Q.defer();
-  console.log('navigating to '+url);
   jsdom.env({
     html: url,
-    src: [zeptoSrc],
+    scripts: this.lib || [],
+    src: this.libSrc || [],
     done: function (errors, window) {
       if (errors) defr.reject(errors);
-      else defr.resolve(window.Zepto, window);
+      else if (!window) defr.reject(new Error('Unable to create window'));
+      else defr.resolve(window);
     }
   });
   return defr.promise;
